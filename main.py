b46544e0fa185f6caf5f47755ccd37c53e50d6bb
jayeshkeshri <zeds@zedss-MacBook-Pro.local>
2024-07-27 18:58:50 +0530

    crosmiddleware

--- main.py
+++ main.py
@@ -4,9 +4,25 @@ import uuid
 from fastapi import FastAPI, Request, HTTPException
 from typing import Dict, Any
 from pydantic import BaseModel
+from starlette.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
+# Define the origins that should be allowed to make cross-origin requests
+origins = [
+    "http://localhost",
+    "http://localhost:3000",
+    "http://localhost:8000",
+    "*"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,            # List of allowed origins
+    allow_credentials=True,           # Allow cookies and credentials
+    allow_methods=["*"],              # Allow all HTTP methods
+    allow_headers=["*"],              # Allow all headers
+)
 class DataObject(BaseModel):
     # Define the structure of your incoming object here
     data: Dict[str, Any]
